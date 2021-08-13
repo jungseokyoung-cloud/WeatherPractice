4281a5f6aed2e1386bcb1f1be7a700e80e57748e
jungseokyoung-cloud <jeongseog-yeong@jeongseog-yeong-ui-MacBook-Pro.local>
2021-08-13 15:03:25 +0900

    ㄴ

--- weatherPractice/weatherPractice/ViewController.swift
+++ weatherPractice/weatherPractice/ViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("호출")
         // Do any additional setup after loading the view.
     }
 
